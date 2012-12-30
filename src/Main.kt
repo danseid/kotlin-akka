d488c0a32264c3fa74acfb858046cfb96da2b34a
Daniel Seidler <daniel.seidler@gmail.com>
2012-12-30 12:15:58 +0100

    code produces Kotlin: [Internal Error] java.lang.AssertionError: Couldn't resolve class

--- src/Main.kt
+++ src/Main.kt
@@ -5,3 +5,10 @@
  * Time: 11:56
  * To change this template use File | Settings | File Templates.
  */
+
+
+import scala.concurrent.duration.Duration
+
+fun main(args: Array<String>) {
+    val duration = Duration.Zero()
+}
